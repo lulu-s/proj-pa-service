b8f706986331aadac33f4ffa0dadc94edfc97564
lulu-s <1192707005@qq.com>
2020-10-13 19:19:44 -0700

    add scroll, logic

--- src/main.js
+++ src/main.js
@@ -3,8 +3,13 @@ import * as YAML from "yaml"
 import Vue from "vue"
 import route from "route-parser"
 import * as ao from "../util"
+// import * as PinyinMatch from "pinyin-match"
 
-// console.log(route);
+
+// 建立数据索引
+// var obj = [];
+// const pinyinEngine = new PinyinEngine(obj);
+
 
 var env = {
     exit: "/exit",
@@ -13,7 +18,9 @@ var env = {
     answer: [],
     ending: false,
     tem: 0,
-    filters: null
+    filters: [],
+    filter_select: null,
+    input_error: false
 }
 window.env = env;
 
@@ -39,6 +46,12 @@ function init(message) {
         // history.state == null && history.pushState(env.que, "", env.que);
     }
 
+    // 建立拼音索引
+    // for(let key in message){
+    //     obj.push(message[key].name)
+    // }
+    // pinyinEngine = new PinyinEngine(obj);
+
     env.selected = message[env.que]
     // env.selected.actions != null && env.selected.actions.forEach(ac => {
     //     ac.select = false;
@@ -52,8 +65,9 @@ function init(message) {
         methods: {
             // button 选择后
             select(v) {
-                // 清空输入框
-                this.$refs.input.value = "";
+                this.$refs.input.value = "";  // 清空输入框
+                env.filters = [];  // 清空筛选器
+                env.filter_select = null;
 
                 // 问答列表新增，history增加， 开启滚动
                 env.que = v;
@@ -64,43 +78,54 @@ function init(message) {
             filter_index() {
                 let value = this.$refs.input.value
                 if (value == "") {
-                    env.filters = null;
+                    env.filter_select = null;
+                    env.filters = [];
                     return;
-                };
+                } else {
+                    // console.log(pinyinEngine.query(value)) 
+                }
                 if (env.selected.actions != null) {
+                    env.filter_select = null;
                     let d = env.selected.actions;
                     for (let i = 0; i < d.length; i++) {
                         if (d[i].title.indexOf(value) > -1) {
-                            env.filters = this.message[d[i].target]
+                            env.filter_select = this.message[d[i].target]
                             return;
                         }
                     }
-                    if (!env.filters) all(this.message);
+                    if (!env.filter_select) all(this.message);
                 } else {
                     all(this.message);
                 }
                 function all(d) {
+                    env.filter_select = null;
+                    env.filters = [];
                     for (let key in d) {
                         if (d[key].name.indexOf(value) > -1) {
-                            env.filters = d[key]
-                            return;
+                            env.filter_select = d[key]
+                            env.filters.push( { key, ...d[key] } )
                         }
                     }
-                    env.filters = null;
                 }
             },
             // button 提交
             input_commit() {
                 let value = this.$refs.input.value;
                 if (value == "") return;
-                if (env.filters != null) {
-                    this.add_answer(env.filters) 
-                    env.filters = null;
+                if (env.filter_select != null) {
+                    this.add_answer(env.filter_select) 
+                    env.filter_select = null;
+                    this.$refs.input.value = ""
                 } else {
                     // TODO 猜你喜欢
-                    this.$refs.input.placeholder = "no result"
+                    this.$refs.input.value = "no result"
+                    env.input_error = true;
+                    let _this = this;
+                    setTimeout(function () {
+                        _this.$refs.input.value = ""
+                        env.input_error = false;
+                    }, 2000)
                 }
-                this.$refs.input.value = ""
             },
             // 问题列表新增
             add_answer(obj) {
